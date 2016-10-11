b15007055c3a05b6453d89196a45192a8d381211
dskam <dskam@users.noreply.github.com>
2016-10-11 15:38:42 -0700

    Added raw and beginning xml values.  Added placeholder code

--- app/src/main/java/com/pnworangecake/aquariumplantapp/ListFragment.java
+++ app/src/main/java/com/pnworangecake/aquariumplantapp/ListFragment.java
@@ -5,11 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class is meant to display a list of selectable items to the user. This list is passed in
- * as a fragment's arguments. Once a plant is selected, the callbacks will return information
- * about the plant to the activity and it is decided how the activity will display the information.
+ * as a fragment's arguments. Once a plants is selected, the callbacks will return information
+ * about the plants to the activity and it is decided how the activity will display the information.
  */
 public class ListFragment extends Fragment {
     @Override
@@ -26,5 +33,19 @@ public class ListFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // From fragment's arguments, retrieve the list of plants to display onto the list view for
         // this fragment.
+        // This step will be replaced by the parser.
+        List<String> plants = new ArrayList<>();
+        plants.add("Plant 1");
+        plants.add("Plant 2");
+        // Add more plants.
+        BaseAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.activity_list_item, plants);
+        ListView list = (ListView) view.findViewById(R.id.plant_list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // Create a PlantFragment by a callback similar to a button in name fragment.
+            }
+        });
     }
 }
--- app/src/main/java/com/pnworangecake/aquariumplantapp/PlantFragment.java
+++ app/src/main/java/com/pnworangecake/aquariumplantapp/PlantFragment.java
@@ -5,10 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Called by a ListFragment or by a random selection on the main fragment. This will display the
- * information on the plant passed in by arguments.
+ * information on the plants passed in by arguments.
  */
 public class PlantFragment extends Fragment {
     @Override
@@ -19,5 +20,15 @@ public class PlantFragment extends Fragment {
     ) {
         super.onCreateView(inflater, container, savedInstanceState);
         return inflater.inflate(R.layout.fragment_plant, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Read the arguments passed into this fragment and generate the view for the user.
+        Bundle args = getArguments();
+        String commonName = "sample common plant name";
+        String scientificName = args.getString("sci-name");
+        ((TextView) view.findViewById(R.id.common_name_header)).setText(commonName);
     }
 }
